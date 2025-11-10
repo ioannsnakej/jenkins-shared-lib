659f2b069031b1f75248f12e6c9c1865e1afa321
Khodyrev Ivan <ioannsnakej@gmail.com>
2025-11-10 15:09:52 +0700

    fix notify

--- vars/notifyTelegram.groovy
+++ vars/notifyTelegram.groovy
@@ -9,7 +9,7 @@ def call(String prefix, String botToken, String chatId) {
         .replace('${buildStatus}', buildStatus)
         .replace('${buildUrl}', buildUrl)
   ssh """
-    curl -s -X POST https://api.telegram.org/bot${botToken}/sendMessage \
+    curl -v -s -X POST https://api.telegram.org/bot${botToken}/sendMessage \
     -d chat_id=${chatId} \
     -d parse_mode=Markdown \
     -d text="${msg.replace("\"", "\\\"")}"
